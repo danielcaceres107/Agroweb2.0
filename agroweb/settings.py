17870af74ab696e0e0e204e207980613df968b6e
Daniel Caceres <dcaceres@servex-us.com>
2023-12-10 17:02:34 -0500

    prueba staticfiles_Dir (?)

--- agroweb/settings.py
+++ agroweb/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = PRODUCTION_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
@@ -165,6 +165,9 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
+    STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite/static')
+    ]
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
