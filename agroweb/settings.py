d84bc8a2537450ca3f4c4a73a8d752b645430bd3
Daniel Caceres <dcaceres@servex-us.com>
2023-12-10 17:24:41 -0500

    trying no whitnoise installed apps

--- agroweb/settings.py
+++ agroweb/settings.py
@@ -55,6 +55,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
